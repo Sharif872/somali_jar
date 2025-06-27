50b49adf6fbc82daba2d1d2446931d9c9eec59ef
Sharif872 <89176345+Sharif872@users.noreply.github.com>
2025-06-27 20:32:25 +0300

    Add files via upload

---
+++ jar/app.js
@@ -0,0 +1,49 @@
+
+//row a
+
+const a = document.querySelector('.a');
+const b = document.querySelector('.b');
+const c = document.querySelector('.c');
+
+// row b
+
+const a2 = document.getElementById('a2');
+const b2 = document.getElementById('b2');       
+const c2 = document.getElementById('c2');
+
+// row c
+const a3 = document.getElementById('a3');
+const b3 = document.getElementById('b3');
+const c3 = document.getElementById('c3');
+
+
+
+// veriables x and o
+
+let x = 'x';
+let o = 'o';
+let winner = null;
+
+function play(){
+    if 
+}
+
+
+
+
+
+
+
+// function hor_a(){
+
+// a.addEventListener('click', () => {
+//     a.innerHTML = 'x';
+// })
+// b.addEventListener('click', () => {
+//     b.innerHTML = 'y';})
+
+// c.addEventListener('click', () => {
+//     c.innerHTML = 'z';})
+
+// }
+// hor_a()
